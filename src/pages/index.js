034881e4253df08ac804190fc99d1b6ec6477a43
Markus Kamp <m.kamp@conterra.de>
2018-11-23 15:23:28 +0100

    add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,17 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Learn to design and code React apps</h1>
-    <p>So lerne ich React Apps zu programmieren.</p>
-    <p>Und noch eine Zeile. hahaha</p>
-  
-    <Link to="/page-2/">Go to page 2 </Link>
-  </Layout>
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>So lerne ich React Apps zu programmieren.</p>
+        <p>Und noch eine Zeile. hahaha</p>
+
+        <Link to="/page-2/">Go to page 2 </Link>
+      </div>
+    </div>
+  </div>
 )
 
 export default IndexPage
